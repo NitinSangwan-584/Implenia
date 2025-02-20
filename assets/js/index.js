aa7245a835e122937e41d1c3fd6bf35569ff44a6
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-02-20 13:59:20 +0530

    video

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,7 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
+document.getElementById('video-thumbnail').addEventListener('click', function () {
+    this.style.display = 'none';
+    document.getElementById('video-frame').style.display = 'block';
+});
