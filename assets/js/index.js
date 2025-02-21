d0b03bf3206da241182d0aa885610d3eeca66154
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-02-21 11:19:21 +0530

    add section

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
-document.getElementById('video-thumbnail').addEventListener('click', function () {
-    this.classList.add('d-none'); // Hide the thumbnail
-    document.getElementById('video-frame').classList.remove('d-none'); // Show the video
-});
+// document.getElementById('video-thumbnail').addEventListener('click', function () {
+//     this.classList.add('d-none'); // Hide the thumbnail
+//     document.getElementById('video-frame').classList.remove('d-none'); // Show the video
+// });
